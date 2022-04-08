manteiga25/renoir

input: touchscreen: focaltech: Optimize interrupt CPU usage
Instead of preventing all CPUs from entering deep idle states, only
force the ones needed for touch processing to stay awake. To process a
touch interrupt, SPI reads are needed which make use of a completion
interrupt from the SPI controller (since SPI I/O is asynchronous). This
interrupt and the touch controller's interrupt are all that need to be
sped up. Only waking up CPUs that process these interrupts significantly
cuts down the overall power consumption of touch interrupt processing.

Additionally, speed up the SPI interrupt by affining it to the big CPU
cluster. SPI interrupts are too numerous to place them on the prime CPU
cluster, so they get the big CPU cluster instead.

Commit description by Sultan Alsawaf <sultan@kerneltoast.com>

Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

drivers/input/touchscreen/focaltech_3658u/focaltech_core.h
@@ -56,6 +56,7 @@
 #include <linux/time.h>
 #include <linux/jiffies.h>
 #include <linux/fs.h>
+#include <linux/pm_qos.h>
 #include <linux/proc_fs.h>
 #include <linux/version.h>
 #include <linux/types.h>
@@ -64,6 +65,8 @@
 #include <linux/dma-mapping.h>
 #include <linux/power_supply.h>
 #include "focaltech_common.h"
+
+#include <linux/spi/spi-geni-qcom.h>
 
 /*****************************************************************************
 * Private constant and macro definitions using #define
@@ -288,6 +291,9 @@ struct fts_ts_data {
 	bool clicktouch_enable;
 	u8 fps_cmd;
 	u8 fps_state;
+
+	struct pm_qos_request pm_spi_req;
+	struct pm_qos_request pm_touch_req;
 };
 
 enum GESTURE_MODE_TYPE {

roxipro/renoir-1

drivers/input/touchscreen/focaltech_3658u/focaltech_core.h
@@ -56,6 +56,7 @@
 #include <linux/time.h>
 #include <linux/jiffies.h>
 #include <linux/fs.h>
+#include <linux/pm_qos.h>
 #include <linux/proc_fs.h>
 #include <linux/version.h>
 #include <linux/types.h>
@@ -64,6 +65,8 @@
 #include <linux/dma-mapping.h>
 #include <linux/power_supply.h>
 #include "focaltech_common.h"
+
+#include <linux/spi/spi-msm-geni.h>
 
 /*****************************************************************************
 * Private constant and macro definitions using #define
@@ -288,6 +291,9 @@ struct fts_ts_data {
 	bool clicktouch_enable;
 	u8 fps_cmd;
 	u8 fps_state;
+
+	struct pm_qos_request pm_spi_req;
+	struct pm_qos_request pm_touch_req;
 };
 
 enum GESTURE_MODE_TYPE {